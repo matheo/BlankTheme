324dd5dc101b82ec59cf7da6416a2e18697a1a8e
Mateo TibaPalacios <nestormateo@gmail.com>
2011-04-29 03:31:03 +0000

    [BlankTheme] Update to YAML 3.3 :D
    git-svn-id: https://code.zikula.org/svn/blanktheme/trunk/themes@499 60a32485-07e5-4ee0-b02d-8fa6578692ca

--- BlankTheme/yaml/core/js/yaml-focusfix.js
+++ BlankTheme/yaml/core/js/yaml-focusfix.js
@@ -1,63 +1,60 @@
 /**
  * "Yet Another Multicolumn Layout" - (X)HTML/CSS Framework
  *
- * (en) Workaround for Webkit browsers to fix focus problems when using skiplinks
- * (de) Workaround für Webkit browsers, um den Focus zu korrigieren, bei Verwendung von Skiplinks
+ * (en) Workaround for IE8 und Webkit browsers to fix focus problems when using skiplinks
+ * (de) Workaround für IE8 und Webkit browser, um den Focus zu korrigieren, bei Verwendung von Skiplinks
  *
  * @note			inspired by Paul Ratcliffe's article 
  *					http://www.communis.co.uk/blog/2009-06-02-skip-links-chrome-safari-and-added-wai-aria
+ *                  Many thanks to Mathias Schäfer (http://molily.de/) for his code improvements
  *
  * @copyright       Copyright 2005-2010, Dirk Jesse
  * @license         CC-A 2.0 (http://creativecommons.org/licenses/by/2.0/),
  *                  YAML-C (http://www.yaml.de/en/license/license-conditions.html)
  * @link            http://www.yaml.de
  * @package         yaml
- * @version         3.2.1
- * @revision        $Revision: 443 $
- * @lastmodified    $Date: 2009-12-31 15:05:05 -0200 (jue 31 de dic de 2009) $
+ * @version         3.3
+ * @revision        $Revision: 466 $
+ * @lastmodified    $Date: 2010-09-14 14:19:30 -0500 (mar 14 de sep de 2010) $
  */
-
-var YAML_focusFix = {
-	init: function() {
+ 
+(function () {
+	var YAML_focusFix = { 
+		skipClass : 'skip',
 		
-		var userAgent = navigator.userAgent.toLowerCase();
-		var	is_webkit = userAgent.indexOf('webkit') > -1;
-		var	is_ie = userAgent.indexOf('msie') > -1;
-		var i = 0;
-		var links, skiplinks = [];
+		init : function () {
+			var userAgent = navigator.userAgent.toLowerCase();
+			var	is_webkit = userAgent.indexOf('webkit') > -1;
+			var	is_ie = userAgent.indexOf('msie') > -1;
+			
+			if (is_webkit || is_ie) {
+				var body = document.body,
+					handler = YAML_focusFix.click;
+				if (body.addEventListener) {
+					body.addEventListener('click', handler, false);
+				} else if (body.attachEvent) {
+					body.attachEvent('onclick', handler);
+				}
+			}
+		},
 		
-		if (is_webkit || is_ie)
-		{
-			// find skiplinks in modern browsers ...
-			if ( document.getElementsByClassName !== undefined) {
-				skiplinks = document.getElementsByClassName('skip');
-	
-				for (i=0; i<skiplinks.length; i++) {
-					this.setTabIndex(skiplinks[i]);
-				}
-			} else {
-				// find skiplinks in older browsers ...
-				links = document.getElementsByTagName('a');
-				for (i=0; i<links.length; i++) {
-					var s = links[i].getAttribute('href');
-					if (s.length > 1 && s.substr(0, 1) == '#' ) {
-						this.setTabIndex(links[i]);				
-					}
-				}
-			}	
+		click : function (e) {
+			e = e || window.event;
+			var target = e.target || e.srcElement;
+			if (target.className.indexOf(YAML_focusFix.skipClass) > -1) {
+				YAML_focusFix.focus(target);
+			}
+		},
+		
+		focus : function (link) {
+			var href = link.href,
+				id = href.substr(href.indexOf('#') + 1),
+				target = document.getElementById(id);
+			if (target) {
+				target.setAttribute("tabindex", "-1");
+				target.focus();
+			}
 		}
-	},
-	
-	setTabIndex: function( skiplink ){
-		var target = skiplink.href.substr(skiplink.href.indexOf('#')+1);
-		var targetElement = document.getElementById(target);
-	
-		if (targetElement !== null) {
-			// make element accessible for .focus() method  
-			targetElement.setAttribute("tabindex", "-1");
-			skiplink.setAttribute("onclick", "document.getElementById('"+target+"').focus();");		
-		}
-	}
-};
-
-YAML_focusFix.init();
+	};
+	YAML_focusFix.init();
+})();
